aa93c6a024b79750089a3c6b74db0ab50bfc2e37
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-10 16:56:35 +0200

    delete every console.log(..)

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const seatContainer = document.querySelector(".seat-container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
-// console.log(seats);
 const count = document.querySelector(".count");
 const total = document.querySelector(".total");
 const movieSelector = document.querySelector("select");
-// console.log(movieSelected);
-// console.log(movieSelected.value);
-//console.log(typeof movieSelected.value);
+
 let ticketPrice = +movieSelector.value;
 
 populateUI();
@@ -14,9 +11,6 @@ populateUI();
 // Update selectd seats and total price
 function updateCountAndTotal() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
-  // console.log(selectedSeats);
-  //here selectedSeats are NodeList.
-  //By using spread operator, make it into an array
 
   const seatsIndex = [...selectedSeats].map(function (seat) {
     return [...seats].indexOf(seat);
@@ -47,18 +41,17 @@ function populateUI() {
     });
   }
   const selectedMovie = localStorage.getItem("selectedMovie");
-  movieSelector.selectedIndex = selectedMovie;
+  if (selectedMovie !== null) {
+    movieSelector.selectedIndex = selectedMovie;
+  }
 }
 
 // EventLister - seat select
 seatContainer.addEventListener("click", (e) => {
-  //console.log(e);
-  //console.log(e.target);
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("occupied")
   ) {
-    //console.log(e.target);
     e.target.classList.toggle("selected");
     updateCountAndTotal();
   }
@@ -66,7 +59,6 @@ seatContainer.addEventListener("click", (e) => {
 
 // EventListener - movie change
 movieSelector.addEventListener("change", (e) => {
-  //   console.log(e.target.value);
   ticketPrice = +e.target.value;
   setMovieData(e.target.selectedIndex, e.target.value);
   updateCountAndTotal();
