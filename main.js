0c9e2e067aec60d817b67ba030e5b796c9c19d12
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-10 15:04:48 +0200

    add eventlistener for clicking seats
    when an unouccuied seat is clicked, add 'selected' class and turn its background color to blue

--- main.js
+++ main.js
@@ -8,3 +8,25 @@ const movieSelected = document.querySelector("select");
 // console.log(movieSelected.value);
 //console.log(typeof movieSelected.value);
 let ticketPrice = +movieSelected.value;
+
+// Update selectd seats and total price
+function updateCountAndTotal() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const newCount = selectedSeats.length;
+  count.textContent = newCount;
+  total.textContent = newCount * ticketPrice;
+}
+
+// EventLister - seat select
+seatContainer.addEventListener("click", (e) => {
+  //console.log(e);
+  //console.log(e.target);
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    //console.log(e.target);
+    e.target.classList.toggle("selected");
+    updateCountAndTotal();
+  }
+});
